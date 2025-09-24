d15c20ea7817baff160df2c9cdda02283d560ecf
Jodie Wattez <jodie.wattez@gmail.com>
2025-09-24 16:29:25 +0200

    feat(pages): ajout login et inscription)

---
+++ src/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/users', name: 'user_list')]
+    public function index(): Response
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        return $this->render('user/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
