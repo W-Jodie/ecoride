1603ec9e2687417c68f888b0b33a03dab62de658
Jodie Wattez <jodie.wattez@gmail.com>
2025-09-26 19:46:47 +0200

    feat/admin : update(création dashboard via un exemple de bootstrap theme/ajout dans dossier templates - base_admin.html.twig, dossier complet admin)

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
+    #[Route('/admin', name: 'admin_dashboard')]
     public function index(): Response
     {
         // $this->denyAccessUnlessGranted('ROLE_ADMIN');
@@ -16,5 +16,22 @@ final class AdminController extends AbstractController
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
+    }
+    #[Route('/admin/charts', name: 'admin_charts')]
+    public function charts(): Response
+    {
+        return $this->render('admin/charts.html.twig');
+    }
+
+    #[Route('/admin/buttons', name: 'admin_buttons')]
+    public function buttons(): Response
+    {
+        return $this->render('admin/buttons.html.twig');
+    }
+
+    #[Route('/admin/tables', name: 'admin_tables')]
+    public function tables(): Response
+    {
+        return $this->render('admin/tables.html.twig');
     }
 }
