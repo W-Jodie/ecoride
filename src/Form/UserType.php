2101f1771c9d23540ff9041ded41d7aa7d95328b
Jodie Wattez <jodie.wattez@gmail.com>
2025-09-27 17:57:12 +0200

    feat/crud-user( integration de CRUD user dans le dossier admin - creation du fichier UserType dans Dossier Form)

---
+++ src/Form/UserType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles', ChoiceType::class, [
+                'choices'  => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+                'expanded' => true,   // checkboxes
+                'multiple' => true,   // plusieurs rôles possibles
+                'label'    => 'Rôles'
+            ])
+            ->add('password', PasswordType::class, [
+                'mapped' => true,
+                'label' => 'Mot de passe',
+                'required' => false, // utile en édition pour laisser vide si pas de changement
+            ])
+            ->add('firstName')
+            ->add('lastName')
+            ->add('isVerified')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
