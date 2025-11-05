79785adae088017529b2de9b5d4853722b81fea6
Jodie Wattez <jodie.wattez@gmail.com>
2025-11-05 18:44:39 +0100

    feat:(Carpooling) ajout du fichier form et show, modification CarpoolingController show

--- src/Controller/Front/Carpooling/CapoolingController.php
+++ src/Controller/Front/Carpooling/CapoolingController.php
@@ -15,4 +15,12 @@ final class CapoolingController extends AbstractController
             'controller_name' => 'CarpoolingController',
         ]);
     }
+
+    #[Route('/front/carpooling/{id}', name: 'app_front_carpooling_show')]
+    public function show(): Response
+    {
+        return $this->render('front/carpooling/show.html.twig', [
+            'controller_name' => 'CarpoolingController',
+        ]);
+    }
 }
