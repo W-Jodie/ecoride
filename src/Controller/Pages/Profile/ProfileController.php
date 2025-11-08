504972b2ec8b34b2f3f4eb58cdae3c7f63233804
Jodie Wattez <jodie.wattez@gmail.com>
2025-11-08 22:49:07 +0100

    refactor/ux: creation de la page profile, fichier ProfileController crée

---
+++ src/Controller/Pages/Profile/ProfileController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Pages\Profile;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CarRepository;
+use App\Repository\TripRepository;
+use App\Repository\WalletRepository;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function index(
+        CarRepository $carRepository, 
+        // TripRepository $tripRepository, 
+        WalletRepository $walletRepository
+    ): Response {
+        $user = $this->getUser();
+
+        // Récupération des voitures de l'utilisateur
+        $cars = $carRepository->findBy(['user' => $user]);
+
+        // Récupération des trajets de l'utilisateur
+        // $trips = $tripRepository->findBy(['driver' => $user]);
+
+        // Récupération du wallet
+        $wallet = $walletRepository->findOneBy(['user' => $user]);
+
+        return $this->render('profile/index.html.twig', [
+            'user' => $user,
+            'cars' => $cars,
+            // 'trips' => $trips,
+            'wallet' => $wallet,
+        ]);
+    }
+}
